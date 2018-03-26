4506b85c29b202f6cb27cf5069b47d6242efa138
patrickatcw <pj8392@yahoo.com>
2018-03-26 15:01:47 -0400

    added gearbox class

---
+++ src/com/me/Gearbox.java
@@ -0,0 +1,53 @@
+package com.me;
+
+import java.util.ArrayList;
+
+//step 1 create this class
+public class Gearbox {
+
+    //step 2 fields
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int currentGear = 0;
+
+    //step 3 constructor
+    public Gearbox(int maxGears) {
+        this.maxGears = maxGears;
+
+        //step 4 initilize this manually
+        this.gears = new ArrayList<>();
+
+        //step 5 instance Gear
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+
+    }
+
+        //step 6 inner class
+        private class Gear {
+
+            //step 7 add fields
+            private int gearNumber;
+            private double ratio;
+
+            //step 8 add constructor
+            public Gear(int gearNumber, double ratio) {
+                this.gearNumber = gearNumber;
+                this.ratio = ratio;
+            }
+
+            //step 9 create method for drivespeed
+            public double driveSpeed(int revs) {
+
+                //step 10 a definition for an inner class
+                return revs * (this.ratio);
+
+                //step 11 go back to main
+
+            }
+
+        }
+
+    }
+
+
