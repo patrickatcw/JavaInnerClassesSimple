fff9bf96f2bf7c101e6181b15d655b5f80e79142
patrickatcw <pj8392@yahoo.com>
2018-03-26 14:30:10 -0400

    initial structure and updated gitignore file

---
+++ src/com/me/Main.java
@@ -0,0 +1,13 @@
+package com.me;
+//https://docs.oracle.com/javase/tutorial/java/javaOO/nested.html
+//improves encapsulation
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
